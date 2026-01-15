956ed18cf82430da14b651b4258b13351cef0d2f
Andrei <andrei.catalin.vrednicu@gmail.com>
2026-01-15 19:22:04 +0100

    rutas front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import './index.css'
+import {Routes, Route} from "react-router-dom"
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 
 function App() {
   
   return (
     <main>
       <Routes>
-        
+        <Route path='/' element={<Home/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </main>
   )
